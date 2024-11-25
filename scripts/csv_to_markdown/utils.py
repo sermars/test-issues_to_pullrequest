24cb22b79a86881a9550a5ffaf3a9f02453d1ef5
Sergio <44024844+sermars@users.noreply.github.com>
2024-11-25 12:11:35 +0000

    fixed minor error with urls

--- scripts/csv_to_markdown/utils.py
+++ scripts/csv_to_markdown/utils.py
@@ -18,26 +18,27 @@ def load_config(config_path: Path) -> Dict[str, Dict]:
         config = yaml.safe_load(file)
     return config
 
-def download_file(url: Path, download_dir:Path, headers: dict = None) -> Path:
+
+def download_file(url: str, download_dir: Path, headers: dict = None) -> Path:
     """
     Downloads a file from the given URL and saves it to the specified directory.
 
     Args:
-        - url (Path): The URL of the file to download.
+        - url (str): The URL of the file to download.
         - download_dir (Path): The directory where the downloaded file will be saved.
-        - headers (dict, optional): A dictionary of HTTP headers to send with the 
+        - headers (dict, optional): A dictionary of HTTP headers to send with the
             request. Defaults to None.
 
     Returns:
         - Path: The path to the downloaded file.
 
     Raises:
-        - requests.exceptions.HTTPError: If the HTTP request returned an 
+        - requests.exceptions.HTTPError: If the HTTP request returned an
             unsuccessful status code.
     """
     response = requests.get(url, headers)
     response.raise_for_status()
-    file_path = download_dir / url.name
-    with open(file_path, 'wb') as file:
+    file_path = download_dir / Path(url).name
+    with open(file_path, "wb") as file:
         file.write(response.content)
     return file_path
